6e781bbb77cd65ec6a3d50b439493940d8a28211
neria uzan <neriauzan@gmail.com>
2023-09-14 18:19:15 +0300

    MP +

--- Player.py
+++ Player.py
@@ -2,30 +2,59 @@ from AOFTable import AOFTable, win32gui
 import time
 from utils_player import find_running_tables_window, set_running_tables
 import torch
-from multiprocessing import Process, Lock
+from torch.multiprocessing import Process, Queue
+import time
 
 
 def run_table(table, lock):
-    pass
+    while True:
+        if table.is_table_visible():
+            table.fg_table()
+            if table.find_button_location(save=True):
+                table.update_hand_counter()
 
+            if table.is_my_turn(save=True):
+                table.find_blinds_location(save=True)
+                table.figure_table_structure()
+                table.figure_state()
+                table.read_holding_cards(save=True)
+
+                print(table)
+                table.act()
 
 
 if __name__ == "__main__":
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = "cpu"
+
     print('Device:', device)
-    dealer_model = torch.load("./trained_Button_model.torch")
-    blinds_model = torch.load("./trained_Blinds_model.torch")
-    action_model = torch.load("./trained_Action_model.torch")
-    suit_model = torch.load("./trained_Suit_model.torch")
-    value_model = torch.load("./trained_Value_model.torch")
+    dealer_model = torch.load("./trained_Button_model.torch").to(device)
+    blinds_model = torch.load("./trained_Blinds_model.torch").to(device)
+    action_model = torch.load("./trained_Action_model.torch").to(device)
+    suit_model = torch.load("./trained_Suit_model.torch").to(device)
+    value_model = torch.load("./trained_Value_model.torch").to(device)
 
     print('Models Loaded')
+    torch.multiprocessing.set_start_method('spawn')
 
     aof_tables_list = find_running_tables_window()
     running_tables = set_running_tables(tables_list=aof_tables_list,
                                         dealer_model=dealer_model, blinds_model=blinds_model, action_model=action_model, value_model=value_model, suit_model=suit_model,
                                         device=device)
+
+    processes = []
+
+    for table in running_tables:
+        process = Process(target=run_table, args=(table, None))
+        process.start()
+        processes.append(process)
+
+    for process in processes:
+        process.join()
+
+
+    exit()
 
     while True:
         for table in running_tables:
